8b6f4f1aa48d9cd3133ed69a1c5db27992991ff1
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-06-07 23:07:29 +0530

    add them

---
+++ loop4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter N: ";
+    cin >> n;
+
+    for(int i = 1; i <= 10; i++)
+    {
+     
+      cout<< n << "x" << i << "=" << n * i<< endl; 
+     
+    }
+ cout<< "  this is table ";
+   
+    return 0;
+}
