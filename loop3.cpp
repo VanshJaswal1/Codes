2476dd661979a8e678f295c4426d054da2eab728
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-06-07 22:42:15 +0530

    add them

---
+++ loop3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n , fact=1;
+
+    cout << "Enter N: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+      fact = fact*i; 
+     
+    }
+ cout<< " fact is = "<< fact;
+   
+    return 0;
+}
---
+++ loop3.java
@@ -0,0 +1,18 @@
+
+
+public class loop3 {
+     public static void main(String[] args) {
+         int i , fact=1;
+
+   
+
+    for( i = 1; i <= 10; i++)
+    {
+      fact = fact*i; 
+     
+    }
+   
+    System.out.println( fact);
+
+     }
+}
