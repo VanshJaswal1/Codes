7d8a69ba851fb0508182433d60214026a85637dd
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-06-07 21:39:36 +0530

    add them

---
+++ loop1.java
@@ -0,0 +1,12 @@
+
+
+public class loop1 {
+    public static void main(String[] args) {
+        int i = 10;
+
+        for( i = 1; i <= 10; i++)
+    { System.out.println(i);
+       
+    }
+    }
+}
