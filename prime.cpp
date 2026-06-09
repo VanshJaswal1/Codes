401721b0ecd51d79fa7fc550180be64721806414
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-06-09 21:33:32 +0530

    add them

---
+++ prime.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+      
+
+int  n ;
+cout<< "enter any number"<<endl;
+cin>> n;
+
+ 
+    if ( n%n ==0 && n%1 == n  ) {
+
+        cout<< " this is a prime number" <<endl;
+        cout<< n<<endl;
+    }
+ 
+ else {
+
+      cout<< " this is not prime number" <<endl;
+    
+ }
+
+
+
+
+    return 0;
+}
